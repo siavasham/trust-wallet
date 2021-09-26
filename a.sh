0be0d33cec0baf6462afc68a92da0a111d394872
siavasham <66677857+siavasham@users.noreply.github.com>
2021-09-26 21:57:46 +0330

    ok

--- a.sh
+++ a.sh
@@ -1,4 +1,8 @@
-for (( c=1; c<=5; c++ ))
-do  
-   echo "Welcome $c times"
+coins=('btc' 'eth')
+for coin in "${coins[@]}"
+do
+    for (( c=1; c<=5; c++ ))
+    do  
+    echo "Welcome $c times $coin"
+    done
 done
