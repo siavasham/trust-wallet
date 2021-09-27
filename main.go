4fb835823b65c4fbb44269e6f59b6fea4c10d44d
siavasham <66677857+siavasham@users.noreply.github.com>
2021-09-27 16:07:55 +0330

    ok

--- main.go
+++ main.go
@@ -1,5 +1,4 @@
 package main
-import "C"
 import "os"
 // #cgo CFLAGS: -I/wallet-core/include
 // #cgo LDFLAGS: -L/wallet-core/build -L/wallet-core/build/trezor-crypto -lTrustWalletCore -lprotobuf -lTrezorCrypto -lc++ -lm
@@ -8,6 +7,7 @@ import "os"
 // #include <TrustWalletCore/TWData.h>
 // #include <TrustWalletCore/TWPrivateKey.h>
 // #include <TrustWalletCore/TWPublicKey.h>
+import "C"
 import "fmt"
 import "unsafe"
 import "encoding/hex"
@@ -115,7 +115,7 @@ func main() {
     wallet := C.TWHDWalletCreateWithMnemonic(str, emtpy)
     defer C.TWHDWalletDelete(wallet)
 
-    var key = C.TWHDWalletGetDerivedKey(wallet, types[coin], 0, 0 ,0 )
+    var key := C.TWHDWalletGetDerivedKey(wallet, types[coin], 0, 0 ,0 )
     keyData := C.TWPrivateKeyData(key)
     keyHex := hex.EncodeToString(TWDataGoBytes(keyData))
     fmt.Println("private key: ", keyHex)
