2acf9789a865c6ffa40389b14ea90dd465f63b21
siavasham <66677857+siavasham@users.noreply.github.com>
2021-09-27 16:40:51 +0330

    ok

--- ok.go
+++ ok.go
@@ -1,11 +1,4 @@
 package main
-#cgo CFLAGS: -I/wallet-core/include
-#cgo LDFLAGS: -L/wallet-core/build -L/wallet-core/build/trezor-crypto -lTrustWalletCore -lprotobuf -lTrezorCrypto -lc++ -lm
-#include <TrustWalletCore/TWHDWallet.h>
-#include <TrustWalletCore/TWString.h>
-#include <TrustWalletCore/TWData.h>
-#include <TrustWalletCore/TWPrivateKey.h>
-#include <TrustWalletCore/TWPublicKey.h>
 import "C"
 import "fmt"
 import "unsafe"
