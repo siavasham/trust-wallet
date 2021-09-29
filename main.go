69fbac97f3d2a48cbd276df37eae9ae06af7ebc6
siavasham <66677857+siavasham@users.noreply.github.com>
2021-09-29 21:27:13 +0330

    ok

--- main.go
+++ main.go
@@ -142,8 +142,9 @@ func main() {
 
     address := C.TWAnyAddressCreateWithPublicKey( pubKey,types[coin])
     defer C.TWAnyAddressDelete(address)
+    addressStr := C.TWStringUTF8Bytes(address)
     // addressStr := C.TWAnyAddressCreateWithString( address,types[coin])
-    fmt.Println("address:", TWStringGoString(address))
+    fmt.Println("address:", TWStringGoString(addressStr))
 
 
 }
