b520d66b04eed8e0f7238f567318f40078265535
siavasham <66677857+siavasham@users.noreply.github.com>
2021-12-21 01:24:09 +0330

    ok

--- sample.cpp
+++ sample.cpp
@@ -99,7 +99,8 @@ int main(int argc,char* argv[]) {
         menomic.replace(startPosition, from.length(), to);
         pos += to.length();
     }
-    auto secretMnemonic = TWStringCreateWithUTF8Bytes(menomic);
+    string secret = TWStringUTF8Bytes(menomic);
+    auto secretMnemonic = TWStringCreateWithUTF8Bytes(secret);
     // walletImp = TWHDWalletCreateWithMnemonic(secretMnemonic, TWStringCreateWithUTF8Bytes(""));
     // TWStringDelete(secretMnemonic);
     
