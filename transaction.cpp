7cdfe7953f7adf5a7d6b0d9a538fd393624ec4f9
siavasham <66677857+siavasham@users.noreply.github.com>
2022-03-18 20:18:46 +0330

    ok

--- transaction.cpp
+++ transaction.cpp
@@ -6,11 +6,13 @@
 #include "json.hpp"
 
 #include <TrustWalletCore/TWCoinType.h>
-#include <TrustWalletCore/TWAnySigner.h>
 #include <TrustWalletCore/TWCoinTypeConfiguration.h>
 #include <TrustWalletCore/TWHDWallet.h>
 #include <TrustWalletCore/TWPrivateKey.h>
 #include <TrustWalletCore/TWString.h>
+
+#include "proto/Tron.pb.h"
+#include <TrustWalletCore/TWAnySigner.h>
 
 #include <iostream>
 #include <string>
@@ -21,7 +23,38 @@ using json = nlohmann::json;
 using namespace std;
 
 int main(int argc,char* argv[]) {
-    json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
-    std::string s = j.dump();   
-    std::cout << j.dump(4) << std::endl;
+     TWHDWallet* walletImp = nullptr;
+    auto menomic =  argv[1];
+    replace( menomic, menomic+strlen(menomic), '-', ' ' );
+    auto secretMnemonic = TWStringCreateWithUTF8Bytes(menomic);
+    walletImp = TWHDWalletCreateWithMnemonic(secretMnemonic, TWStringCreateWithUTF8Bytes(""));
+    TWStringDelete(secretMnemonic);
+    
+    const TWCoinType coinType = TWCoinType::TWCoinTypeTron;
+    int userId = 9;
+    TWPrivateKey* privateKey = TWHDWalletGetKeyBIP44(walletImp, coinType, 0, 0,0);
+    string source = TWStringUTF8Bytes(TWCoinTypeDeriveAddress(coinType, privateKey));
+
+    TWPrivateKey* privateKey2 = TWHDWalletGetKeyBIP44(walletImp, coinType, 0, 0,userId);
+    string dest = TWStringUTF8Bytes(TWCoinTypeDeriveAddress(coinType, privateKey2));
+
+    TWHDWalletDelete(walletImp);
+
+    auto secretPrivKey = TWPrivateKeyData(privateKey);
+
+    auto input = Proto::SigningInput();
+    auto& transaction = *input.mutable_transaction();
+
+    auto& transfer = *transaction.mutable_transfer_asset();
+    transfer.set_owner_address(source);
+    transfer.set_to_address(dest);
+    transfer.set_amount(5);
+
+    input.set_private_key(secretPrivKey.bytes.data(), secretPrivKey.bytes.size());
+
+    const auto output = Signer::sign(input);
+
+    auto signedTransaction = string(TWStringUTF8Bytes(output));
+    cout  << signedTransaction << endl;
+    
 }
